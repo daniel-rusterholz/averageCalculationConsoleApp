067c9a7a7d20d0e9c4c76d1a0884da7be23f5799
Daniel <dr@axicode.dk>
2017-07-21 12:06:43 +0200

    Created input validation and average calculation

--- averageCalculation/Program.cs
+++ averageCalculation/Program.cs
@@ -10,7 +10,47 @@ namespace averageCalculation
     {
         static void Main()
         {
+            var totalNumber = 0.0;
+            var totalEntries = 0.0;
 
+            while (true)
+            { 
+
+                Console.Write("Enter a number or type \"done\" to see the average:");
+                var entry = Console.ReadLine();
+
+                try
+                {
+                    if (entry.ToLower() == "done")
+                    {
+                        var calculation = totalNumber/totalEntries;
+
+                        Console.WriteLine("Your average is: " + calculation);
+                        break;
+                    }
+
+                    totalNumber += double.Parse(entry);
+
+                    if (Double.Parse(entry) <= 0)
+                    {
+                        Console.WriteLine("0 is not a valid input!");
+                        continue;
+                    }
+
+                    Console.WriteLine("You have entered: " + entry + " and your total is: " + totalNumber);
+
+                    totalEntries += 1.0;
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Input is not valid");
+                    continue;
+                }
+
+            }
+
+            Console.WriteLine("Goodbye");
 
         }
     }
